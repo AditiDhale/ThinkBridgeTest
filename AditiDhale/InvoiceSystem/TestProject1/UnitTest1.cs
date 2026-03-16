07572cf54e0e93c513b6975c3c9317de0a6225cc
AditiDhale <aditidhale@gmail.com>
2026-03-16 16:22:59 +0530

    Tried adding NUnit Project

--- AditiDhale/InvoiceSystem/TestProject1/UnitTest1.cs
+++ AditiDhale/InvoiceSystem/TestProject1/UnitTest1.cs
@@ -1,15 +1,27 @@
-﻿namespace TestProject1
+﻿using UsageBillingApp.Models;
+using UsageBillingApp.Services;
+
+namespace TestProject1
 {
+    [TestFixture]
     public class Tests
     {
+        private InvoiceCalculator _calcService;
+
         [SetUp]
         public void Setup()
         {
+            _calcService = new InvoiceCalculator();
         }
 
         [Test]
         public void Test1()
         {
+            // Arrange
+            UsageRecord record = new UsageRecord();
+            // Act
+            var actualResult = _calcService.Calculate(record);
+            // Assert
             Assert.Pass();
         }
     }
